72302764ab8fb01b0db0a2b3a6e338d8e6b0fa22
Rahul Kandel <rahulkandel7@gmail.com>
2023-02-10 16:43:41 +0545

    added footer

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import App from "./App";
+import WholeSellerLayout from "./wholeseller/layouts/WholeSellerLayout";
 import Login from "./wholeseller/pages/login";
 import Register from "./wholeseller/pages/Register";
 
 export default function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<WholeSellerLayout />}>
+                <Route index element={<App />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Route>
+
         </Routes>
     )
 }
---
+++ src/wholeseller/layouts/WholeSellerLayout.jsx
@@ -0,0 +1,48 @@
+import { AiOutlineUser } from 'react-icons/ai';
+import { GoSearch } from 'react-icons/go';
+import { GrFavorite } from 'react-icons/gr';
+import { VscSignOut } from 'react-icons/vsc';
+import { Outlet } from "react-router";
+import { Footer } from '../components/Footer';
+
+function WholeSellerLayout() {
+    return (<>
+        {/* Desktop Nav Bar Code */}
+        <div className="w-11/12 mx-auto pt-2">
+            <div className="flex gap-10 items-center justify-between">
+                {/* Logo of the app */}
+                <div>
+                    <img src="./logo.png" alt="Multivendor logo" className='w-52 mt-0' />
+                </div>
+                {/* Search Box */}
+                <div className=' flex-1 items-center justify-center flex'>
+                    <div className='relative w-9/12' >
+                        <div className='absolute top-[50%] -translate-y-[50%] left-[2%]'>
+                            <GoSearch className='text-gray-600' />
+                        </div>
+                        <input type="search" name="" id="" className="bg-gray-100 border-gray-100 pl-8 pr-4 outline-none focus-visible:outline-none rounded-full py-1 shadow-sm focus:border-gray-600 active:border-gray-600 placeholder-shown:text-sm w-full" placeholder="Looking for something..." />
+                    </div>
+                </div>
+                {/* Others icons of the app */}
+                <div className='flex gap-5'>
+                    {/* Wishlist Icon */}
+                    <div className='flex item-center h-full justify-center'>
+                        <GrFavorite className='mt-1' /> <p className='pl-2'>Wishlist</p>
+                    </div>
+                    {/* Profile Icon */}
+                    <div className='flex item-center h-full justify-center'>
+                        <AiOutlineUser className='mt-1' /> <p className='pl-2'>Profile</p>
+                    </div>
+                    {/* Logout Icon */}
+                    <div className='flex item-center h-full justify-center'>
+                        <VscSignOut className='mt-1' /> <p className='pl-2'>Logout</p>
+                    </div>
+                </div>
+            </div>
+            <Outlet />
+        </div>
+        <Footer />
+    </>);
+}
+
+export default WholeSellerLayout;
