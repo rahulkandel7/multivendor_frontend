6f8c10e4a38039c80fc83fef043a2668b4b024e7
Rahul Kandel <rahulkandel7@gmail.com>
2023-02-13 12:15:12 +0545

    make navbar responsive

--- src/wholeseller/layouts/WholeSellerLayout.jsx
+++ src/wholeseller/layouts/WholeSellerLayout.jsx
@@ -1,6 +1,8 @@
 import { AiOutlineUser } from 'react-icons/ai';
+import { BiCategoryAlt } from 'react-icons/bi';
 import { GoSearch } from 'react-icons/go';
 import { GrFavorite } from 'react-icons/gr';
+import { RiHome2Line } from 'react-icons/ri';
 import { VscSignOut } from 'react-icons/vsc';
 import { Outlet } from "react-router";
 import { Link } from 'react-router-dom';
@@ -14,12 +16,12 @@ function WholeSellerLayout() {
                 {/* Logo of the app */}
                 <div>
                     <Link to="/">
-                        <img src="./logo.png" alt="Multivendor logo" className='w-52 mt-0' />
+                        <img src="./logo.png" alt="Multivendor logo" className='w-32 md:w-52 mt-0' />
                     </Link>
                 </div>
                 {/* Search Box */}
                 <div className=' flex-1 items-center justify-center flex'>
-                    <div className='relative w-9/12' >
+                    <div className='relative md:w-9/12 w-full' >
                         <div className='absolute top-[50%] -translate-y-[50%] left-[2%]'>
                             <GoSearch className='text-gray-600' />
                         </div>
@@ -27,7 +29,7 @@ function WholeSellerLayout() {
                     </div>
                 </div>
                 {/* Others icons of the app */}
-                <div className='flex gap-5'>
+                <div className='hidden md:flex gap-5'>
                     {/* Wishlist Icon */}
                     <div className='flex item-center h-full justify-center'>
                         <GrFavorite className='mt-1' /> <p className='pl-2'>Wishlist</p>
@@ -43,6 +45,35 @@ function WholeSellerLayout() {
                 </div>
             </div>
             <Outlet />
+        </div>
+
+        {/* Mobile navigation */}
+        <div className=' md:hidden fixed bottom-0 w-full bg-white shadow-md z-10'>
+            <div className='flex gap-1 justify-around items-center px-6 py-2'>
+
+                <Link to='/' className=' flex flex-col items-center text-gray-900  px-2 py-3 '>
+                    <RiHome2Line />
+                    <p className='mt-1'>
+                        Home
+                    </p>
+                </Link>
+
+                <Link to='/' className=' flex flex-col items-center text-gray-500  px-2 py-3 '>
+                    <BiCategoryAlt />
+                    <p className='mt-1'>Category</p>
+                </Link>
+
+                <Link to='/' className=' flex flex-col items-center text-gray-500  px-2 py-3 '>
+                    <GrFavorite />
+                    <p className='mt-1'>Wishlist</p>
+                </Link>
+
+                <Link to='/' className=' flex flex-col items-center text-gray-500  px-2 py-3 '>
+                    <AiOutlineUser />
+                    <p className='mt-1'>Profile</p>
+                </Link>
+
+            </div>
         </div>
         <Footer />
     </>);
